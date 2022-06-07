7e507e0dcdfda2d94e638554d1acb6634a43f6b2
Filipe Varela <43725698+filipe-varela@users.noreply.github.com>
2022-06-07 16:14:20 +0100

    Deleted module to replace with submodule

--- Android/hswatch_connection_module/src/main/java/io/hackerschool/hswatch_connection_module/connection_objects/IHSWProtocolSender.java
+++
@@ -1,7 +0,0 @@
-package io.hackerschool.hswatch_connection_module.connection_objects;
-
-import java.util.List;
-
-public interface IHSWProtocolSender {
-    List<String> protocolSenderCallback();
-}
